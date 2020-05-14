f98864f5f872745351fca1eb24cd8926deefade2
李程俊 <183542728@qq.com>
2020-05-14 21:09:33 +0800

    feat: 重写fitView,保持原始比例，x轴居中对齐，并保证root节点在y轴顶部
    fix #12

--- src/components/RelationMap/index.vue
+++ src/components/RelationMap/index.vue
@@ -27,13 +27,35 @@ export default {
     const graph = editor.create();
     graph.data(data);
     graph.render();
-    graph.fitView();
     this.graph = graph;
+    this.fitViewTop(20);
     setTimeout(() => {
       this.save();
     }, 5000);
   },
   methods: {
+    // 重写fitView,保持原始比例，x轴居中对齐，并保证root节点在y轴顶部
+    fitViewTop(yOffset) {
+      const { graph } = this;
+
+      const width = graph.get("width");
+      const height = graph.get("height");
+      const group = graph.get("group");
+      group.resetMatrix();
+      const bbox = group.getCanvasBBox();
+
+      if (bbox.width === 0 || bbox.height === 0) return;
+      const viewCenter = {
+        x: width / 2,
+        y: height / 2,
+      };
+
+      const groupCenter = {
+        x: bbox.x + bbox.width / 2,
+        y: bbox.y + bbox.height / 2,
+      };
+      graph.translate(viewCenter.x - groupCenter.x, groupCenter.y + yOffset);
+    },
     handleNameClick: (event) => {
       console.log("handleNameClick", event);
     },
