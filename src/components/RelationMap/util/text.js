bdfa4f3c93bb17d8b05df69d669e2a8d58761132
leecj <183542728@qq.com>
2020-05-15 17:17:28 +0800

    feat: 文本居中的计算规则，加入中英文区分
    fix #14

--- src/components/RelationMap/util/text.js
+++ src/components/RelationMap/util/text.js
@@ -1,4 +1,15 @@
 export function getOffset(leftText, rightText, baseSize = 5.8) {
-  const difference = rightText.length - leftText.length;
+  const rightChinese = getChinese(rightText);
+  const rightLength =
+    (rightText.length - rightChinese.length) / 2 + rightChinese.length;
+
+  const leftChinese = getChinese(leftText);
+  const leftLength =
+    (leftText.length - leftChinese.length) / 2 + leftChinese.length;
+
+  const difference = rightLength - leftLength;
   return difference * baseSize;
+}
+function getChinese(str) {
+  return str.match(/[\u4e00-\u9fa5]/g) || [];
 }
